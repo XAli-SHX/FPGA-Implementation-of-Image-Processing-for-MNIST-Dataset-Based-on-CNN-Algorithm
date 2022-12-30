6948d910dfded66297b41c07c2cc2fb690f41851
Ali Shayanpoor <shayanpoorali66@gmail.com>
2022-12-30 14:59:53 +0330

    Add log prints
    print hidden_units & accuracy after each calculation

--- Codes/MNIST MLP Training/python/List_Accuracy_with_hidden_units.py
+++ Codes/MNIST MLP Training/python/List_Accuracy_with_hidden_units.py
@@ -7,7 +7,13 @@ def main():
     csvFile = open('hidden_units accuracy.csv', 'w')
     csvFile.write("hidden_units,accuracy\n")
     while acc < 0.98:
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        print(f"hidden_units: {hidden_units}")
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
         acc = train(hidden_units)
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        print(f"accuracy: {acc}")
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
         csvFile.write(f"{hidden_units},{acc}\n")
         hidden_units += 1
     csvFile.close()
