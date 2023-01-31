7bab6deaa1bbcdaa7bc0bef24f5c6349e95f8001
Ali Shayanpoor <56270506+XAli-SHX@users.noreply.github.com>
2023-01-31 12:34:11 +0330

    Create predict.py

---
+++ Codes/software/mnist_cnn_training/predict.py
@@ -0,0 +1,66 @@
+from tensorflow import keras
+from PIL import Image, ImageOps
+import numpy as np
+
+
+def loadModel(path):
+    return keras.models.load_model(path, compile=True)
+
+
+def loadImage(path) -> list:
+    original = Image.open(path)
+    # original.show("original")
+    grayscale = ImageOps.grayscale(original)
+    grayscale = ImageOps.flip(grayscale)
+    grayscale = grayscale.rotate(-90)
+    # grayscale.show("grayscale")
+    size = (28, 28)
+    mnistCompatible = grayscale.resize(size)
+    mnistCompatibleInv = ImageOps.invert(mnistCompatible)
+    # mnistCompatibleInv.save("img0_inv.png")
+    print(mnistCompatibleInv.size)
+    pixels = []
+    for x in range(28):
+        for y in range(28):
+            pixels.append(mnistCompatibleInv.getpixel((x, y)))
+    pixels = np.array(pixels).astype('float32') / 255
+    pixels = pixels.reshape([1, 28, 28, -1])
+    return pixels
+
+
+def predict(model, img: list, printOutputs: bool = False) -> int:
+    print(model)
+    num = model.predict(img)[0]
+    if printOutputs:
+        print("outputs percents: [")
+        for i in range(len(num)):
+            print(f"\t{i}: {num[i]:.4f}")
+        print("]")
+    maxIndex = 0
+    maxVal = num[maxIndex]
+    for i in range(10):
+        if num[i] > maxVal:
+            maxVal = num[i]
+            maxIndex = i
+    return maxIndex
+
+
+def main(modelPath="trained_model.h5", imgPath="tests/handwrite_black_pixel28x28/img7.png"):
+    model = loadModel(path=modelPath)
+    img = loadImage(path=imgPath)
+
+    # from keras.datasets import mnist
+    # (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    # img = x_test[0]
+    # dim = img.shape[0]
+    # flat_test = x_test.reshape([-1, dim * dim])
+    # norm_test = flat_test.astype('float32') / 255
+    # sample = norm_test[0].reshape([1, -1])
+
+    num = predict(model, img, True)
+    print(f"predicted number = {num}")
+    return num
+
+
+if __name__ == "__main__":
+    main()
