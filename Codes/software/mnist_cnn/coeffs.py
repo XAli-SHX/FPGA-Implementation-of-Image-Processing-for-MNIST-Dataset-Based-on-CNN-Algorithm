9780a42131c091727458868f925ef50f7335599d
Ali Shayanpoor <56270506+XAli-SHX@users.noreply.github.com>
2023-02-06 14:19:04 +0330

    Save layer coeffs in file

--- Codes/software/mnist_cnn/coeffs.py
+++ Codes/software/mnist_cnn/coeffs.py
@@ -19,8 +19,21 @@ model = keras.Sequential(
 
 if __name__ == "__main__":
     model = keras.models.load_model("./trained_model.h5", compile=True)
+
     layer0_conv2d = model.layers[0]
-    layer0_file = open("./coeffs/layer0_conv2d.txt", "w")
-    layer0_file.write(str(layer0_conv2d.weights))
-    layer0_file.close()
+    layer2_conv2d = model.layers[2]
+    layer6_dense = model.layers[6]
+
+    file = open(f"./coeffs/layer0_conv2d.txt", "w")
+    file.write(str(layer0_conv2d.weights))
+    file.close()
+
+    file = open(f"./coeffs/layer2_conv2d.txt", "w")
+    file.write(str(layer2_conv2d.weights))
+    file.close()
+
+    file = open(f"./coeffs/layer6_dense.txt", "w")
+    file.write(str(layer6_dense.weights))
+    file.close()
+
     print("hi")
