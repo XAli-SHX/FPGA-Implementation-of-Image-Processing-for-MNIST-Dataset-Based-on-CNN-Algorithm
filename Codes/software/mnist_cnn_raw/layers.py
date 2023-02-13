695a344046bb8fc8b8dbbe438e4b0b1e61b62c3c
Ali Shayanpoor <56270506+XAli-SHX@users.noreply.github.com>
2023-02-13 14:56:07 +0330

    Update layers.py

--- Codes/software/mnist_cnn_raw/layers.py
+++ Codes/software/mnist_cnn_raw/layers.py
@@ -4,14 +4,15 @@ import numpy as np
 def conv2d(inputImage, inputShape, kernel, kernelShape):
     WIDTH = inputShape[0]
     HEIGHT = inputShape[1]
+    CHANNEL_IN = inputShape[2]
     KX_SIZE = kernelShape[0]
     KY_SIZE = kernelShape[1]
-    CHANNEL_IN = kernelShape[2]
-    CHANNEL_OUT = kernelShape[3]
+    CHANNEL_OUT = kernelShape[2]
 
     outputImage = np.zeros((HEIGHT, WIDTH, CHANNEL_OUT))
 
     for y in range(HEIGHT):
+        print(f"progress = {int(y / HEIGHT * 100)}%")
         for x in range(WIDTH):
             for ky in range(KY_SIZE):
                 for kx in range(KX_SIZE):
@@ -20,9 +21,10 @@ def conv2d(inputImage, inputShape, kernel, kernelShape):
                             if ((y + ky) >= HEIGHT) or ((x + kx) >= WIDTH):
                                 continue
                             kr = kernel[0][ky][kx][chIn][chOut]
-                            px = inputImage[y + ky][x + kx][chIn]
-                            outputImage[y][x][chOut] += kr * px
+                            # print(f"x = {x}, y = {y}, chIn = {chIn}, kx = {kx}, ky = {ky}, chOut = {chOut}")
+                            px = inputImage[chIn][y + ky][x + kx]
+                            outputImage[chOut][y][x] += kr * px
                         bias = kernel[1][chOut]
-                        outputImage[y][x][chOut] += bias
+                        outputImage[chOut][y][x] += bias
 
     return outputImage
