a2ceb072ecfe98a00fdbc258f5d30356a3312dd2
Ali <Ali@Alis-MacBook-Pro.local>
2023-03-08 11:36:45 +0400

    Add layers
    -ReLU \n-Complete Conv2D \n-maxPooling2d \n-flatten \n-dropout

--- Codes/software/mnist_cnn_raw/layers.py
+++ Codes/software/mnist_cnn_raw/layers.py
@@ -1,24 +1,36 @@
 import numpy as np
+import random
 
+def ReLU(image: list, shape: tuple):
+    width = shape[0]
+    height = shape[1]
+    channelIn = shape[2]
+    out = np.zeros((channelIn, height, width))
+    for y in range(height):
+        for x in range(width):
+            for chIn in range(channelIn):
+                data = image[chIn][y][x]
+                out[chIn][y][x] = data * (data > 0)
+    return out.tolist()
 
-def conv2d(inputImage, inputShape, kernel, kernelShape):
-    WIDTH = inputShape[0]
-    HEIGHT = inputShape[1]
-    CHANNEL_IN = inputShape[2]
-    KX_SIZE = kernelShape[0]
-    KY_SIZE = kernelShape[1]
-    CHANNEL_OUT = kernelShape[2]
+def conv2d(inputImage, inputShape, kernel, kernelShape) -> list:
+    width = inputShape[0]
+    height = inputShape[1]
+    channelIn = inputShape[2]
+    kxSize = kernelShape[0]
+    kySize = kernelShape[1]
+    channelOut = kernelShape[2]
 
-    outputImage = np.zeros((CHANNEL_OUT, HEIGHT, WIDTH))
+    outputImage = np.zeros((channelOut, height, width))
 
-    for y in range(HEIGHT):
-        print(f"progress = {int(y / HEIGHT * 100)}%")
-        for x in range(WIDTH):
-            for ky in range(KY_SIZE):
-                for kx in range(KX_SIZE):
-                    for chOut in range(CHANNEL_OUT):
-                        for chIn in range(CHANNEL_IN):
-                            if ((y + ky) >= HEIGHT) or ((x + kx) >= WIDTH):
+    for y in range(height):
+        # print(f"progress = {int(y / height * 100)}%")
+        for x in range(width):
+            for ky in range(kySize):
+                for kx in range(kxSize):
+                    for chOut in range(channelOut):
+                        for chIn in range(channelIn):
+                            if ((y + ky) >= height) or ((x + kx) >= width):
                                 continue
                             kr = kernel[0][ky][kx][chIn][chOut]
                             # print(f"x = {x}, y = {y}, chIn = {chIn}, kx = {kx}, ky = {ky}, chOut = {chOut}")
@@ -27,4 +39,46 @@ def conv2d(inputImage, inputShape, kernel, kernelShape):
                         bias = kernel[1][chOut]
                         outputImage[chOut][y][x] += bias
 
-    return outputImage
+    return outputImage.tolist()
+
+def maxPooling2d(inputImage: list, shape: tuple, poolSize: tuple):
+    width = shape[0]
+    height = shape[1]
+    channelIn = shape[2]
+    pxSize = poolSize[0]
+    pySize = poolSize[1]
+    outImage = np.zeros((channelIn, height // pySize, width // pxSize))
+    for y in range(0, height, pySize):
+        for x in range(0, width, pxSize):
+            for chIn in range(channelIn): 
+                max = inputImage[chIn][y][x]
+                for py in range(pySize):
+                    for px in range(pxSize):
+                        if ((y + py) >= height) or ((x + px) >= width):
+                                continue
+                        if inputImage[chIn][y + py][x + px] > max:
+                            max = inputImage[chIn][y + py][x + px]
+                outImage[chIn][y // pySize][x // pxSize] = max
+    return outImage.tolist()
+
+def flatten(inputImage: list) -> list:
+    arr = np.array(inputImage)
+    return arr.flatten().tolist()
+
+def dropout(inputImageFlat: list, rate: float) -> list:
+    out = np.zeros(len(inputImageFlat))
+    for i in range(len(inputImageFlat)):
+        if random.uniform(0, 1) < rate:
+            out[i] = 0
+        else:
+            out[i] = inputImageFlat[i]
+    return inputImageFlat
+
+# def dense(inputImageFlat: list, weights: list, shape: tuple) -> list:
+#     inLen = shape[0]
+#     outLen = shape[1]
+#     mulWeights = weights[0]
+#     biasWeights = weights[1]
+#     out = np.zeros(outLen)
+#     for i in range(inLen):
+#         inputImageFlat[i] * mulWeights
