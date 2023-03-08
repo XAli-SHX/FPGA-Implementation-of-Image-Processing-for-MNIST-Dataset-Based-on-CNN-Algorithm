fb680ab0b78a46be3e077d9e9bd4654885161b48
Ali <Ali@Alis-MacBook-Pro.local>
2023-03-08 11:41:03 +0400

    Add layer 0 to layer 5 for test

--- Codes/software/mnist_cnn_raw/predict.py
+++ Codes/software/mnist_cnn_raw/predict.py
@@ -1,8 +1,21 @@
 from tensorflow import keras
 import numpy as np
 import show1dImage as show1d
-import layers
+from layers import ReLU, conv2d, dropout, flatten, maxPooling2d
+import json
 
+def show1dGrayImage(imgArr, xSize, ySize, xTraverse=True):
+    from PIL import Image
+    img = Image.new('L', (xSize, ySize))
+    if xTraverse:
+        for x in range(xSize):
+            for y in range(ySize):
+                img.putpixel((x, y), int(imgArr[x * ySize + y] * 255))
+    else:
+        for x in range(xSize):
+            for y in range(ySize):
+                img.putpixel((x, y), int(imgArr[y * xSize + x] * 255))
+    img.show()
 
 def _loadTestData():
     # Load the data and split it between train and test sets
@@ -17,22 +30,75 @@ def _loadTestData():
     testModel = x_test[0].reshape([1, 28, 28, -1])
     return testModel
 
+def _loadWeightsFromJson(path: str) -> list:
+    with open(path, 'r') as fp:
+        data = json.load(fp)
+        fp.close()
+    return data
 
 def main():
     model = keras.models.load_model("./trained_model.h5", compile=True)
-    layer0_conv2d = model.layers[0]
-    layer2_conv2d = model.layers[2]
-    layer6_dense = model.layers[6]
-    layer0_conv2d_weights = layer0_conv2d.weights
-    layer2_conv2d_weights = layer2_conv2d.weights
-    layer6_dense_weights = layer6_dense.weights
+    layer0_conv2d_weights = _loadWeightsFromJson('./weights/json/layer0_conv2d.json')
+    layer2_conv2d_weights = _loadWeightsFromJson('./weights/json/layer2_conv2d.json')
+    layer6_dense_weights = _loadWeightsFromJson('./weights/json/layer6_dense.json')
     testModel = _loadTestData()
-    show1d.show1dGrayImage(testModel.flatten(), 28, 28, False)
+    show1dGrayImage(testModel.flatten(), 28, 28, False)
     expected = model.predict(testModel)
     print(f"expected: {expected}")
-    layer0Out = layers.conv2d(testModel, (28, 28, 1), layer0_conv2d_weights, (3, 3, 32))
-    print(layer0Out)
 
+    # layer 0 --> conv2d
+    # in shape = (28, 28, 1)
+    # out shape = (26, 26, 32)
+    layer0Out = conv2d(testModel, (28, 28, 1), layer0_conv2d_weights, (3, 3, 32))
+    print(f"layer0Out: {layer0Out}")
+
+    # layer 0 activation function
+    # in shape = (28, 28, 1)
+    # out shape = (26, 26, 32)
+    layer0ReLU = ReLU(layer0Out, (26, 26, 32))
+    print(f"layer0ReLU: {layer0ReLU}")
+
+    # layer 1 --> max pooling 2d
+    # in shape = (26, 26, 32)
+    # out shape = (13, 13, 32)
+    layer1Out = maxPooling2d(layer0ReLU, (26, 26, 32), (2, 2))
+    print(f"layer1Out: {layer1Out}")
+
+    # layer 2 --> conv2d
+    # in shape = (13, 13, 32)
+    # out shape = (11, 11, 64)
+    layer2Out = conv2d(layer1Out, (13, 13, 32), layer2_conv2d_weights, (3, 3, 64))
+    print(f"layer2Out: {layer2Out}")
+
+    # layer 2 activation function
+    # in shape = (13, 13, 32)
+    # out shape = (11, 11, 64)
+    layer2ReLU = ReLU(layer2Out, (11, 11, 64))
+    print(f"layer2ReLU: {layer2ReLU}")
+
+    # layer 3 --> max pooling 2d
+    # in shape = (11, 11, 64)
+    # out shape = (5, 5, 64)
+    layer3Out = maxPooling2d(layer2ReLU, (11, 11, 64), (2, 2))
+    print(f"layer3Out: {layer3Out}")
+
+    # layer 4 --> flatten
+    # in shape = (5, 5, 64)
+    # out shape = 1600
+    layer4Flatten = flatten(layer3Out)
+    print(f"layer4Flatten: {layer4Flatten}")
+    
+    # layer 5 --> Dropout
+    # in shape = 1600
+    # out shape = 1600
+    layer5Dropout = dropout(layer4Flatten, 0.5)
+    print(f"layer5Dropout: {layer5Dropout}")
+
+    # layer 6 --> Dense (fully connected)
+    # in shape = 1600
+    # out shape = 10
+    print(len(layer5Dropout))
+    # layer6Dense = dense(layer5Dropout, layer6_dense_weights, (1600, 10))
 
 if __name__ == "__main__":
     main()
