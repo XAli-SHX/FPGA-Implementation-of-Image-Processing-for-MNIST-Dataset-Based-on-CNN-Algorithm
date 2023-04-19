3abd31266184752d314a48c11a2ddf6418d31286
Ali Shayanpoor <56270506+XAli-SHX@users.noreply.github.com>
2023-04-19 18:06:12 +0330

    Convert layers to flat

--- Codes/software/mnist_cnn_hardware_friendly/predict.py
+++ Codes/software/mnist_cnn_hardware_friendly/predict.py
@@ -9,32 +9,32 @@ def actualAnswer(layer0_conv2d_weights, layer2_conv2d_weights, layer6_dense_weig
     # layer 0 -> conv2d
     # in shape = (28, 28, 1)
     # out shape = (26, 26, 32)
-    layer0Out = conv2d(testModel[0], (28, 28, 1), layer0_conv2d_weights, (3, 3, 32))
+    layer0Out = conv2dFlat(testModel, (28, 28, 1), layer0_conv2d_weights, (3, 3, 32))
     # print(f"layer0Out: {layer0Out}")
     # layer 0 activation function -> ReLU
     # in shape = (26, 26, 32)
     # out shape = (26, 26, 32)
-    layer0ReLU = ReLU(layer0Out, (26, 26, 32))
+    layer0ReLU = reluFlat(layer0Out, (26, 26, 32))
     # print(f"layer0ReLU: {layer0ReLU}")
     # layer 1 -> max pooling 2d
     # in shape = (26, 26, 32)
     # out shape = (13, 13, 32)
-    layer1Out = maxPooling2d(layer0ReLU, (26, 26, 32), (2, 2))
+    layer1Out = maxPooling2dFlat(layer0ReLU, (26, 26, 32), (2, 2))
     # print(f"layer1Out: {layer1Out}")
     # layer 2 -> conv2d
     # in shape = (13, 13, 32)
     # out shape = (11, 11, 64)
-    layer2Out = conv2d(layer1Out, (13, 13, 32), layer2_conv2d_weights, (3, 3, 64))
+    layer2Out = conv2dFlat(layer1Out, (13, 13, 32), layer2_conv2d_weights, (3, 3, 64))
     # print(f"layer2Out: {layer2Out}")
     # layer 2 activation function -> ReLU
     # in shape = (11, 11, 64)
     # out shape = (11, 11, 64)
-    layer2ReLU = ReLU(layer2Out, (11, 11, 64))
+    layer2ReLU = reluFlat(layer2Out, (11, 11, 64))
     # print(f"layer2ReLU: {layer2ReLU}")
     # layer 3 -> max pooling 2d
     # in shape = (11, 11, 64)
     # out shape = (5, 5, 64)
-    layer3Out = maxPooling2d(layer2ReLU, (11, 11, 64), (2, 2))
+    layer3Out = maxPooling2dFlat(layer2ReLU, (11, 11, 64), (2, 2))
     # print(f"layer3Out: {layer3Out}")
     # layer 4 -> flatten
     # in shape = (5, 5, 64)
@@ -49,7 +49,7 @@ def actualAnswer(layer0_conv2d_weights, layer2_conv2d_weights, layer6_dense_weig
     # layer 6 -> Dense (fully connected)
     # in shape = 1600
     # out shape = 10
-    layer6Dense = dense(layer5Dropout, layer6_dense_weights, (1600, 10))
+    layer6Dense = denseFlat(layer5Dropout, layer6_dense_weights, (1600, 10))
     # print(f"layer6Dense: {layer6Dense}")
     # layer 6 activation function -> softmax
     # in shape = 10
@@ -78,7 +78,7 @@ def main():
          loadWeightsFromJson('./weights/flat/layer2_conv2d_biases_flatten.json')],
         [loadWeightsFromJson('./weights/flat/layer6_dense_weights_flatten.json'),
          loadWeightsFromJson('./weights/flat/layer6_dense_biases_flatten.json')],
-        testModel
+        testModel.flatten()
     )
     print(f"actual hardware friendly: {actualAns}")
 
