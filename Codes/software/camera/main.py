3205129f264fac47cb52da66eb6fa1bd5c2980ac
Ali Shayanpoor <56270506+XAli-SHX@users.noreply.github.com>
2023-05-05 13:08:43 +0330

    Add on_image_saved function to predict captured frame

--- Codes/software/camera/main.py
+++ Codes/software/camera/main.py
@@ -1,11 +1,16 @@
 from camera import Camera
+import predict
+
+
+def on_image_saved(path):
+    predict.main(imgPath=path)
 
 
 def main():
-    camera = Camera()
+    camera = Camera(on_image_saved)
     camera.start_camera(has_windows=True)
     camera.await_first_frame()
-    camera.save_image()
+    # predict.main(imgPath="picture.png")
 
 
 if __name__ == "__main__":
