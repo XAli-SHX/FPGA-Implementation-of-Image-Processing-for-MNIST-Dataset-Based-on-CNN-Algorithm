046b52adbac6273fba1af5a9fc8b00242ff3332f
Ali Shayanpoor <56270506+XAli-SHX@users.noreply.github.com>
2023-06-04 10:43:09 +0330

    Create Mux2 module

---
+++ Codes/hardware/hdl/mux2.v
@@ -0,0 +1,14 @@
+module Mux2 #(parameter SIZE) (
+    data0,
+    data1,
+    sel,
+    out
+);
+
+    input [SIZE-1:0] data0, data1;
+    input sel;
+    output [SIZE-1:0] out;
+
+    assign out = sel ? data1 : data0;
+    
+endmodule
