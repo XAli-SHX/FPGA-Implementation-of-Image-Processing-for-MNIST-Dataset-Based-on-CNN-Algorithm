eebac539269fcc48d03b7a664ac8ae5fc76056d9
Ali Shayanpoor <56270506+XAli-SHX@users.noreply.github.com>
2023-06-19 21:22:19 +0330

    Add interface general purpose signals

--- Codes/hardware/hdl/axis_interface.v
+++ Codes/hardware/hdl/axis_interface.v
@@ -1,10 +1,7 @@
-module AxisInterface #(parameter DATAWIDTH=32, INPDATANUM=8, OUTDATANUM=4) (
+module AxisInterface #(parameter DATA_WIDTH=32, IN_DATA_NUM=8, OUT_DATA_NUM=4) (
 	// Base signals
 	clk,
 	rst_n,
-	// Starting signals
-	ex_start,
-	ex_startAck,
 	// Slave side
 	s_data,
 	s_valid,
@@ -15,18 +12,19 @@ module AxisInterface #(parameter DATAWIDTH=32, INPDATANUM=8, OUTDATANUM=4) (
 	m_valid,
 	m_last,
 	m_ready,
-	// AIU side
-	waitSt,
-	waitFin,
-	inp_adr,
-	inp_data,
-	out_adr,
-	out_data
+	// Wrapper side
+	axisif_start,
+	axisif_done,
+	axisif_bufferIn_adr,
+	axisif_bufferIn_data,
+	axisif_bufferOut_adr,
+	axisif_bufferOut_data,
+	axisif_bufferOut_wr
 );
 
 ////// PARAMS
-localparam					INPADRWIDTH		= $clog2(INPDATANUM);
-localparam					OUTADRWIDTH		= $clog2(OUTDATANUM);
+localparam					IN_ADR_WIDTH		= $clog2(IN_DATA_NUM);
+localparam					OUT_ADR_WIDTH		= $clog2(OUT_DATA_NUM);
 
 
 
@@ -35,196 +33,181 @@ localparam					OUTADRWIDTH		= $clog2(OUTDATANUM);
 // Base signals
 input						clk;
 input						rst_n;
-// Starting signals
-input						ex_start;
-output						ex_startAck;
 // Slave side
-input	[DATAWIDTH-1:0]		s_data;
+input	[DATA_WIDTH-1:0]	s_data;
 input						s_valid;
 input						s_last;
 output						s_ready;
 // Master side
-output	[DATAWIDTH-1:0]		m_data;
+output	[DATA_WIDTH-1:0]	m_data;
 output						m_valid;
 output						m_last;
 input						m_ready;
-// AIU side
-output						waitSt;
-input						waitFin;
-input	[INPADRWIDTH-1:0]	inp_adr;
-output	[DATAWIDTH-1:0]		inp_data;
-output	[OUTADRWIDTH-1:0]	out_adr;
-input	[DATAWIDTH-1:0]		out_data;
-
-
-
-////// INTERNAL SIGNALS
-
-// Input Buffer signals
-reg		[DATAWIDTH-1:0]		buf_r_mem[0:INPDATANUM-1];
-wire	[INPADRWIDTH-1:0]	buf_i_adr;
-wire						buf_i_rd;								// UNUSED
-wire						buf_i_wr;
-wire	[DATAWIDTH-1:0]		buf_i_data;
-wire	[DATAWIDTH-1:0]		buf_o_data;
-
-// Input Address Counter signals
-wire						ict_i_clear;
-wire						ict_i_en;
-reg		[INPADRWIDTH-1:0]	ict_r_value;
-wire						ict_o_fin;
-
-// Output Address Counter signals
-wire						oct_i_clear;
-wire						oct_i_en;
-reg		[OUTADRWIDTH-1:0]	oct_r_value;
-wire						oct_o_fin;
-
-// States
-localparam	IDLE		= 0,
-			START		= 1,
-			SLAVE		= 2,
-			WAIT		= 3,
-			MASTER		= 4;
-localparam	STATES		= 5;
-localparam	STATEBITS	= $clog2(STATES);
-
-// State variables
-reg	[STATEBITS-1:0]	con_r_ps;
-reg	[STATEBITS-1:0]	con_r_ns;
-
-// State signals
-wire	con_i_exStart;
-wire	con_i_sValid;
-wire	con_i_ictFin;
-wire	con_i_octFin;
-wire	con_i_waitFin;
-wire	con_i_mReady;
-reg		con_o_clear;
-reg		con_o_startAck;
-reg		con_o_sReady;
-reg		con_o_wr;
-reg		con_o_ictEn;
-reg		con_o_octEn;
-reg		con_o_waitSt;
-reg		con_o_mValid;
-reg		con_o_rd;
-reg		con_o_mLast;
+// Wrapper side
+output						axisif_start;
+input						axisif_done;
+input	[IN_ADR_WIDTH-1:0]	axisif_bufferIn_adr;
+output	[DATA_WIDTH-1:0]	axisif_bufferIn_data;
+input	[OUT_ADR_WIDTH-1:0]	axisif_bufferOut_adr;
+input	[DATA_WIDTH-1:0]	axisif_bufferOut_data;
+input						axisif_bufferOut_wr;
 
 
 
 ////// LOGIC
 
-// Input Buffer logic
-integer i;
-always @(posedge clk, negedge rst_n) begin
-	if (~rst_n) begin
-		for (i = 0; i < INPDATANUM; i = i + 1) begin
-			buf_r_mem[i] <= 0;
-		end
-	end
-	else begin	// clk
-		if (buf_i_wr) begin
-			buf_r_mem[buf_i_adr] <= buf_i_data;
-		end
-	end
-end
-assign buf_o_data = buf_r_mem[buf_i_adr];
+// Input Buffer
+wire	[IN_ADR_WIDTH-1:0]	bufferIn_in_adr;
+wire						bufferIn_in_wr;
+wire	[DATA_WIDTH-1:0]	bufferIn_in_data;
+wire	[DATA_WIDTH-1:0]	bufferIn_out_data;
 
-// Input Address Counter logic
-always @(posedge clk, negedge rst_n) begin
-	if (~rst_n) begin
-		ict_r_value <= 0;
-	end
-	else begin	// clk
-		if (ict_i_clear) begin
-			ict_r_value <= 0;
-		end
-		else if (ict_i_en) begin
-			ict_r_value <= ict_r_value + 1;
-		end
-	end
-end
-assign ict_o_fin = (ict_r_value == INPDATANUM - 1);
+Buffer #(DATA_WIDTH, IN_DATA_NUM) BufferIn
+(
+	.clk(clk),
+	.wr(bufferIn_in_wr),
+	.adr(bufferIn_in_adr),
+	.dataIn(bufferIn_in_data),
+	.dataOut(bufferIn_out_data)
+);
 
-// Output Address Counter logic
-always @(posedge clk, negedge rst_n) begin
-	if (~rst_n) begin
-		oct_r_value <= 0;
-	end
-	else begin	// clk
-		if (oct_i_clear) begin
-			oct_r_value <= 0;
-		end
-		else if (oct_i_en) begin
-			oct_r_value <= oct_r_value + 1;
-		end
-	end
-end
-assign oct_o_fin = (oct_r_value == OUTDATANUM - 1);
+// Output Buffer
+wire	[OUT_ADR_WIDTH-1:0]	bufferOut_in_adr;
+wire						bufferOut_in_wr;
+wire	[DATA_WIDTH-1:0]	bufferOut_in_data;
+wire	[DATA_WIDTH-1:0]	bufferOut_out_data;
+
+Buffer #(DATA_WIDTH, OUT_DATA_NUM) BufferOut
+(
+	.clk(clk),
+	.wr(bufferOut_in_wr),
+	.adr(bufferOut_in_adr),
+	.dataIn(bufferOut_in_data),
+	.dataOut(bufferOut_out_data)
+);
+
+// Input Address Counter
+wire						cntInAdr_in_clear;
+wire						cntInAdr_in_en;
+wire	[IN_ADR_WIDTH-1:0]	cntInAdr_out_val;
+wire						cntInAdr_out_fin;
+
+Counter #(DATA_WIDTH, IN_DATA_NUM - 1) cntInAdr
+(
+	.clk(clk),
+	.rst_n(rst_n),
+	.en(cntInAdr_in_en),
+	.val(cntInAdr_out_val),
+	.fin(cntInAdr_out_fin)
+);
+
+// Output Address Counter
+wire						cntOutAdr_in_clear;
+wire						cntOutAdr_in_en;
+wire	[OUT_ADR_WIDTH-1:0]	cntOutAdr_out_val;
+wire						cntOutAdr_out_fin;
+
+Counter #(DATA_WIDTH, OUT_DATA_NUM - 1) cntInAdr
+(
+	.clk(clk),
+	.rst_n(rst_n),
+	.en(cntOutAdr_in_en),
+	.val(cntOutAdr_out_val),
+	.fin(cntOutAdr_out_fin)
+);
 
 
 
 ////// CONTROLLER
 
-// con_r_ps
+// States
+localparam	IDLE		= 0,
+			SLAVE		= 1,
+			INFORM		= 2,
+			WAIT		= 3,
+			MASTER		= 4,
+			STATES		= 5;
+localparam	STATE_WIDTH	= $clog2(STATES);
+
+// State variables
+reg	[STATE_WIDTH-1:0]	ps;
+reg	[STATE_WIDTH-1:0]	ns;
+
+// State signals
+wire	cu_in_sValid;
+wire	cu_in_cntInAdrFin;
+wire	cu_in_cntOutAdrFin;
+wire	cu_in_axisifDone;
+wire	cu_in_mReady;
+reg		cu_out_sReady;
+reg		cu_out_clear;
+reg		cu_out_wr;
+reg		cu_out_cntInAdrEn;
+reg		cu_out_cntOutAdrEn;
+reg		cu_out_axisifStart;
+reg		cu_out_mValid;
+reg		cu_out_mLast;
+reg		cu_out_workingWithBuffers;
+
+// ps
 always @(posedge clk, negedge rst_n) begin
 	if (~rst_n) begin
-		con_r_ps <= IDLE;
+		ps <= IDLE;
 	end
 	else begin	// clk
-		con_r_ps <= con_r_ns;
+		ps <= ns;
 	end
 end
 
-// con_r_ns
-always @(con_r_ps, con_i_exStart, con_i_sValid,
-		con_i_ictFin, con_i_octFin,	con_i_waitFin,
-		con_i_mReady) begin
-	case (con_r_ps)
-		IDLE:	con_r_ns =	(con_i_exStart)?	START:
-												IDLE;
-		START:	con_r_ns =	(~con_i_exStart)?	SLAVE:
-												START;
-		SLAVE:	con_r_ns =	(~con_i_sValid)?	SLAVE:
-							(con_i_ictFin)?		WAIT:
+// ns
+always @(*) begin
+	case (ps)
+		IDLE:	ns =							SLAVE;
+		SLAVE:	ns =	(~cu_in_sValid)?		SLAVE:
+						(cu_in_cntInAdrFin)?	INFORM:
 												SLAVE;
-		WAIT:	con_r_ns =	(con_i_waitFin)?	MASTER:
+		INFORM:	ns =							WAIT;
+		WAIT:	ns =	(cu_in_axisifDone)?		MASTER:
 												WAIT;
-		MASTER:	con_r_ns =	(~con_i_mReady)?	MASTER:
-							(con_i_octFin)?		IDLE:
+		MASTER:	ns =	(~cu_in_mReady)?		MASTER:
+						(cu_in_cntOutAdrFin)?	SLAVE:
 												MASTER;
 	endcase
 end
 
-// con_o_...
-always @(con_r_ps, con_i_exStart, con_i_sValid,
-		con_i_ictFin, con_i_octFin,	con_i_waitFin,
-		con_i_mReady) begin
-	{con_o_clear, con_o_startAck, con_o_sReady,
-	con_o_wr, con_o_ictEn, con_o_octEn, con_o_waitSt,
-	con_o_mValid, con_o_rd, con_o_mLast} = 0;
-	case (con_r_ps)
-		IDLE:	begin
-			con_o_clear		= 1;
-		end
-		START:	begin
-			con_o_startAck	= 1;
+// Controller outputs
+always @(*) begin
+	{
+		cu_out_sReady,
+		cu_out_clear,
+		cu_out_wr,
+		cu_out_cntInAdrEn,
+		cu_out_cntOutAdrEn,
+		cu_out_axisifStart,
+		cu_out_mValid,
+		cu_out_mLast,
+		cu_out_workingWithBuffers
+	} = 9'b0;
+	case (ps)
+		IDLE: begin
+			cu_out_clear				= 1;
 		end
 		SLAVE:	begin
-			con_o_sReady	= 1;
-			con_o_wr		= con_i_sValid;
-			con_o_ictEn		= con_i_sValid;
+			cu_out_sReady				= 1;
+			cu_out_wr					= cu_in_sValid;
+			cu_out_cntInAdrEn			= cu_in_sValid;
+			cu_out_workingWithBuffers	= 1;
 		end
-		WAIT:	begin
-			con_o_waitSt	= 1;
+		INFORM:	begin
+			cu_out_axisifStart			= 1;
+			cu_out_clear				= 1;
 		end
 		MASTER:	begin
-			con_o_mValid	= 1;
-			con_o_rd		= con_i_mReady;
-			con_o_octEn		= con_i_mReady;
-			con_o_mLast		= (con_i_mReady
-								& con_i_octFin);
+			cu_out_mValid				= 1;
+			cu_out_cntOutAdrEn			= cu_in_mReady;
+			cu_out_mLast				= (cu_in_mReady
+											& cu_in_cntOutAdrFin);
+			cu_out_workingWithBuffers	= 1;
 		end
 	endcase
 end
@@ -233,35 +216,28 @@ end
 ////// CONNECTIONS
 
 // Input Buffer inputs
-assign buf_i_adr		=	con_o_wr ? ict_r_value : inp_adr;
-assign buf_i_rd			=	con_o_rd;
-assign buf_i_wr			=	con_o_wr;
-assign buf_i_data		=	s_data;
+assign bufferIn_in_adr		=	cu_out_workingWithBuffers ? cntInAdr_out_val : axisif_bufferIn_adr;
+assign bufferIn_in_wr		=	cu_out_wr;
+assign bufferIn_in_data		=	s_data;
 
-// Input Address Counter inputs
-assign ict_i_clear		=	con_o_clear;
-assign ict_i_en			=	con_o_ictEn;
+// Output Buffer inputs
+assign bufferOut_in_adr		=	cu_out_workingWithBuffers ? cntOutAdr_out_val : axisif_bufferOut_adr;
+assign bufferOut_in_wr		=	cu_out_workingWithBuffers ? cu_out_wr : axisif_bufferOut_wr;
+assign bufferOut_in_data	=	axisif_bufferOut_data;
 
-// Output Address Counter inputs
-assign oct_i_clear		=	con_o_clear;
-assign oct_i_en			=	con_o_octEn;
+// Input Address Counter input
+assign cntInAdr_in_clear	=	cu_out_clear;
+assign cntInAdr_in_en		=	cu_out_cntInAdrEn;
+
+// Output Address Counter input
+assign cntOutAdr_in_clear	=	cu_out_clear;
+assign cntOutAdr_in_en		=	cu_out_cntOutAdrEn;
 
 // Controller inputs
-assign con_i_exStart	=	ex_start;
-assign con_i_sValid		=	s_valid;
-assign con_i_ictFin		=	ict_o_fin;
-assign con_i_octFin		=	oct_o_fin;
-assign con_i_waitFin	=	waitFin;
-assign con_i_mReady		=	m_ready;
-
-// Module outputs
-assign ex_startAck		=	con_o_startAck;
-assign s_ready			=	con_o_sReady;
-assign m_data			=	out_data;
-assign m_valid			=	con_o_mValid;
-assign m_last			=	con_o_mLast;
-assign waitSt			=	con_o_waitSt;
-assign inp_data			=	buf_o_data;
-assign out_adr			=	oct_r_value;
+assign cu_in_sValid			=	s_valid;
+assign cu_in_cntInAdrFin	=	cntInAdr_out_fin;
+assign cu_in_cntOutAdrFin	=	cntOutAdr_out_fin;
+assign cu_in_axisifDone		=	axisif_done;
+assign cu_in_mReady			=	m_ready;
 
 endmodule
