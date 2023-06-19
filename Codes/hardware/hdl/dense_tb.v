89aa458833c2be44a06fd0b60421e5e25ecb0a12
Ali Shayanpoor <56270506+XAli-SHX@users.noreply.github.com>
2023-06-19 21:30:00 +0330

    Instantiate and wire all top modules in testbench

--- Codes/hardware/hdl/dense_tb.v
+++ Codes/hardware/hdl/dense_tb.v
@@ -2,26 +2,45 @@ module TB_Dense ();
     
     localparam IN_COUNT = 10, OUT_COUNT = 3, DATA_SIZE = 16;
 
-    reg clk = 0, rst = 0, valid = 0;
-    reg [DATA_SIZE-1:0] dataIn = {DATA_SIZE{1'b0}};
-    wire busy, valid;
+    reg clk = 0, rst = 0;
     wire [DATA_SIZE-1:0] weightData, biasData;
-    wire [DATA_SIZE-1:0] dataOut;
     wire [$clog2(IN_COUNT*OUT_COUNT)-1:0] weightAdr;
     wire [$clog2(OUT_COUNT)-1:0] biasAdr;
 
-    Dense #(IN_COUNT, OUT_COUNT, DATA_SIZE) DenseUnit (
+    wire axisif_start; 
+    wire axisif_done; 
+    wire axisif_bufferIn_adr; 
+    wire axisif_bufferIn_data; 
+    wire axisif_bufferOut_adr; 
+    wire axisif_bufferOut_data; 
+    wire axisif_bufferOut_wr;
+
+    reg     s_axis_data;
+    reg     s_axis_valid;
+    reg     s_axis_last;
+    wire    s_axis_ready;
+
+    wire    m_axis_data;
+    wire    m_axis_valid;
+    wire    m_axis_last;
+    reg     m_axis_ready;
+
+    Dense #(IN_COUNT, OUT_COUNT, DATA_SIZE) denseUnit (
         .clk(clk), 
-        .rst(rst), 
-        .valid(valid),
-        .dataIn(dataIn),
-        .weightData(weightData),
-        .biasData(biasData),
-        .busy(busy),
-        .valid(valid),
-        .dataOut(dataOut),
+        .rst(rst),
+        // External weights and biases
         .weightAdr(weightAdr),
-        .biasAdr(biasAdr)
+        .weightData(weightData),
+        .biasAdr(biasAdr),
+        .biasData(biasData),
+        // AXIS Interface
+        .axisif_start(axisif_start),
+        .axisif_done(axisif_done),
+        .axisif_bufferIn_adr(axisif_bufferIn_adr),
+        .axisif_bufferIn_data(axisif_bufferIn_data),
+        .axisif_bufferOut_adr(axisif_bufferOut_adr),
+        .axisif_bufferOut_data(axisif_bufferOut_data),
+        .axisif_bufferOut_wr(axisif_bufferOut_wr)
     );
 
     Ram #(DATA_SIZE, IN_COUNT * OUT_COUNT) weightsRam (
@@ -42,6 +61,30 @@ module TB_Dense ();
         .dataOut(biasData)
     );
 
+    AxisInterface #(DATA_SIZE, IN_COUNT, OUT_COUNT) axisIf (
+        // Base signals
+        .clk(clk),
+        .rst_n(~rst),
+        // Slave side
+        .s_data(s_data),
+        .s_valid(s_valid),
+        .s_last(s_last),
+        .s_ready(s_ready),
+        // Master side
+        .m_data(m_data),
+        .m_valid(m_valid),
+        .m_last(m_last),
+        .m_ready(m_ready),
+        // Wrapper side
+        .axisif_start(axisif_start),
+        .axisif_done(axisif_done),
+        .axisif_bufferIn_adr(axisif_bufferIn_adr),
+        .axisif_bufferIn_data(axisif_bufferIn_data),
+        .axisif_bufferOut_adr(axisif_bufferOut_adr),
+        .axisif_bufferOut_data(axisif_bufferOut_data),
+        .axisif_bufferOut_wr(axisif_bufferOut_wr)
+    );
+
     always #3 clk = ~clk;
 
     initial begin
@@ -52,14 +95,6 @@ module TB_Dense ();
         #10;
         rst = 0;
         #10;
-        valid = 1;
-        #10;
-        valid = 0;
-        repeat (IN_COUNT) begin
-            @(posedge clk);
-            dataIn = $random;
-        end
-        @(negedge valid);
         #20 $stop;
     end
 
